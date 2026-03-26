8e85281d5c508bfa2e7eeaa7e46f0e6c0efc924b
Mangel2007 <nadavlokuendo@gmail.com>
2026-03-26 18:57:37 -0500

    Se definieron los modelos en la clase Vehiculo.java

--- src/main/java/com/uniquindio/migueldiaz/modelo/Vehiculo.java
+++ src/main/java/com/uniquindio/migueldiaz/modelo/Vehiculo.java
@@ -2,4 +2,12 @@ package com.uniquindio.migueldiaz.modelo;
 // CLASE PADRE
 
 public class Vehiculo {
+    String placa;
+    String tipoVehiculo;
+    String nombreConductor;
+    String identificacionConductor;
+    String horaIngreso;
+    String espacioAsignado;
+    String estado;
+    
 }
