76f3f36840bab739d68389723c34d57c973ba158
Mangel2007 <nadavlokuendo@gmail.com>
2026-03-26 19:51:04 -0500

    se creo el respectivo enum de TipoVehiculo.java

--- src/main/java/com/uniquindio/migueldiaz/enums/TipoVehiculo.java
+++ src/main/java/com/uniquindio/migueldiaz/enums/TipoVehiculo.java
@@ -1,4 +1,19 @@
 package com.uniquindio.migueldiaz.enums;
 
 public class TipoVehiculo {
+
+    public enum TipoDeVehiculo {
+        CARRO, MOTOCICLETA, BICICLETA
+    }
+
+    TipoDeVehiculo carro;
+
+    public TipoVehiculo() {
+        this.carro = TipoDeVehiculo.CARRO;
+    }
+
+    public TipoDeVehiculo getCarro() {
+        return carro;
+    }
+
 }
