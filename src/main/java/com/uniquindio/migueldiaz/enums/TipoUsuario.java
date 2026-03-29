49a06c2fe694dd4adf33829c661321e74d764ac7
Mangel2007 <nadavlokuendo@gmail.com>
2026-03-29 16:24:49 -0500

    Se conectó con éxito el Enum TipoUsuario.java con la clase Usuario.java

--- src/main/java/com/uniquindio/migueldiaz/enums/TipoUsuario.java
+++ src/main/java/com/uniquindio/migueldiaz/enums/TipoUsuario.java
@@ -1,26 +1,5 @@
 package com.uniquindio.migueldiaz.enums;
 
-public class TipoUsuario {
-
-    enum TipoDeUsuario {
+    public enum TipoUsuario {
         ESTUDIANTE, DOCENTE, ADMINISTRATIVO, VISITANTE
     }
-
-    // Asignación de abreviaturas
-    private static TipoDeUsuario administrativo = TipoDeUsuario.ADMINISTRATIVO;
-    private static TipoDeUsuario docente = TipoDeUsuario.DOCENTE;
-    private static TipoDeUsuario estudiante = TipoDeUsuario.ESTUDIANTE;
-    private static TipoDeUsuario visitante = TipoDeUsuario.VISITANTE;
-
-    // Creación de clase que contenga enum TipoUsuario
-    public static class EnumTipoDeUsuarios {
-        private TipoDeUsuario tipoDeUsuario;
-
-
-    // Constructor de enum TipoDeUsuario
-
-    public EnumTipoDeUsuarios(TipoDeUsuario tipoDeUsuario){
-        this.tipoDeUsuario = tipoDeUsuario;
-        }
-    }
-}
--- src/main/java/com/uniquindio/migueldiaz/modelo/Usuario.java
+++ src/main/java/com/uniquindio/migueldiaz/modelo/Usuario.java
@@ -1,19 +1,20 @@
 package com.uniquindio.migueldiaz.modelo;
 import com.uniquindio.migueldiaz.enums.TipoVehiculo;
+import com.uniquindio.migueldiaz.enums.TipoUsuario;
 
 public class Usuario {
     protected String nombre;
     protected String identificacion;
-    protected String tipoUsuario;
+    protected TipoUsuario tipoUsuario;
     public String vehiculo;
     public TipoVehiculo tipoVehiculo;
     protected Boolean aplicaDescuento;
 
     // Constructor
-    public Usuario (String nombreRecibido, String identificacionRecibido, String tipoUsuarioRecibido, String vehiculoRecibido, TipoVehiculo tipoVehiculo, Boolean aplicaDescuentoVerificar){
+    public Usuario (String nombreRecibido, String identificacionRecibido, TipoUsuario tipoUsuario, String vehiculoRecibido, TipoVehiculo tipoVehiculo, Boolean aplicaDescuentoVerificar){
         this.nombre = nombreRecibido;
         this.identificacion = identificacionRecibido;
-        this.tipoUsuario = tipoUsuarioRecibido;
+        this.tipoUsuario = tipoUsuario;
         this.vehiculo = vehiculoRecibido;
         this.tipoVehiculo = tipoVehiculo;
         this.aplicaDescuento = aplicaDescuentoVerificar;
